ce626acf43a0b8830b64120deb30de96b1436b72
Joe P Shoulak <joe@squarecompass.io>
2022-11-08 14:12:43 -0600

    asymmetric circuit tiles implemented

--- sketch.js
+++ sketch.js
@@ -1,5 +1,6 @@
-const GRID_SIZE = 40;
-const path = "tiles/demo/";
+const GRID_SIZE = 10;
+// const path = "tiles/demo/";
+const path = "tiles/circuit/";
 let images;
 let grid;
 
@@ -20,8 +21,18 @@ function drawCell(cell) {
 
 function preload() {
   images = {
-    BLANK: loadImage(path + "blank.png"),
-    DOWN: loadImage(path + "down.png"),
+    BLANK_DARK_GRAY: loadImage(path + "0.png"),
+    BLANK_GREEN: loadImage(path + "1.png"),
+    POINT_GREEN: loadImage(path + "2.png"),
+    STRAIGHT_GRAY: loadImage(path + "3.png"),
+    //
+    STRAIGHT_GREEN: loadImage(path + "6.png"),
+    GRAY_O_GREEN: loadImage(path + "7.png"),
+    GRAY_TO_GREEN: loadImage(path + "8.png"),
+    JOINT_GREEN: loadImage(path + "9.png"),
+    GREEN_D_ANGLE: loadImage(path + "10.png"),
+    GREEN_S_ANGLE: loadImage(path + "11.png"),
+    STRAIGHT_G_P: loadImage(path + "12.png"),
   };
 }
 
@@ -40,14 +51,53 @@ function setup() {
     return newImg;
   };
 
-  const BLANK = new Tile(images.BLANK, ["0", "0", "0", "0"]);
-  Cell.options.push(BLANK);
+  // Dark Gray: 0
+  // Dark Green: 1
+  // Light Green: 2
+  // Light Gray: 3
 
-  const DOWN = new Tile(images.DOWN, ["0", "1", "1", "1"]);
-  Cell.options.push(DOWN);
-  Cell.options.push(DOWN.rotate(1));
-  Cell.options.push(DOWN.rotate(2));
-  Cell.options.push(DOWN.rotate(3));
+  Cell.options.push(new Tile(images.BLANK_DARK_GRAY, ["0", "0", "0", "0"]));
+  Cell.options.push(new Tile(images.BLANK_GREEN, ["1", "1", "1", "1"]));
+
+  const POINT_GREEN = new Tile(images.POINT_GREEN, ["1", "2", "1", "1"]);
+  Cell.options.push(POINT_GREEN);
+  Cell.options.push(POINT_GREEN.rotate(1));
+  Cell.options.push(POINT_GREEN.rotate(2));
+  Cell.options.push(POINT_GREEN.rotate(3));
+
+  const STRAIGHT_GRAY = new Tile(images.STRAIGHT_GRAY, ["0", "3", "0", "3"]);
+  Cell.options.push(STRAIGHT_GRAY);
+  Cell.options.push(STRAIGHT_GRAY.rotate(1));
+
+  const STRAIGHT_GREEN = new Tile(images.STRAIGHT_GREEN, ["1", "2", "1", "2"]);
+  Cell.options.push(STRAIGHT_GREEN);
+  Cell.options.push(STRAIGHT_GREEN.rotate(1));
+
+  const GRAY_O_GREEN = new Tile(images.GRAY_O_GREEN, ["3", "2", "3", "2"]);
+  Cell.options.push(GRAY_O_GREEN);
+  Cell.options.push(GRAY_O_GREEN.rotate(1));
+
+  const GRAY_TO_GREEN = new Tile(images.GRAY_TO_GREEN, ["3", "1", "2", "1"]);
+  Cell.options.push(GRAY_TO_GREEN);
+  Cell.options.push(GRAY_TO_GREEN.rotate(1));
+
+  const GREEN_D_ANGLE = new Tile(images.GREEN_D_ANGLE, ["2", "2", "2", "2"]);
+  Cell.options.push(GREEN_D_ANGLE);
+  Cell.options.push(GREEN_D_ANGLE.rotate(1));
+
+  const GREEN_S_ANGLE = new Tile(images.GREEN_S_ANGLE, ["2", "2", "1", "1"]);
+  Cell.options.push(GREEN_S_ANGLE);
+  Cell.options.push(GREEN_S_ANGLE.rotate(1));
+
+  const STRAIGHT_G_P = new Tile(images.STRAIGHT_G_P, ["1", "2", "1", "2"]);
+  Cell.options.push(STRAIGHT_G_P);
+  Cell.options.push(STRAIGHT_G_P.rotate(1));
+
+  const JOINT_GREEN = new Tile(images.JOINT_GREEN, ["2", "2", "1", "2"]);
+  Cell.options.push(JOINT_GREEN);
+  Cell.options.push(JOINT_GREEN.rotate(1));
+  Cell.options.push(JOINT_GREEN.rotate(2));
+  Cell.options.push(JOINT_GREEN.rotate(3));
 
   grid = new Grid(GRID_SIZE, GRID_SIZE);
   grid.resetCallback = (g) => drawGrid(g);
